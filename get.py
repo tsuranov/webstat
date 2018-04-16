3d805b3dceb0558fe39d811e4eebfb24210a782a
root <root@localhost.localdomain>
2018-04-16 22:44:26 +0500

    lenta.ru rss

--- get.py
+++ get.py
@@ -1,7 +1,15 @@
 
 import feedparser
 
-print "Start read RSS"
-d = feedparser.parse('https://www.reddit.com/r/Python/.rss')
+print "Start read RSS Lenta.ru"
+d = feedparser.parse('https://lenta.ru/rss')
 
-print d
+print d['feed']['title']
+print d['feed']['link']
+# print d['feed']['published']
+print d['feed']['description']
+#d.entries[0]
+print len(d.entries)
+
+for item in d.entries:
+    print item['title']
