6397e8b6c7844cd007184fe4070d94f23f6e1def
asd3 <tsuranov@yandex.ru>
2018-04-17 18:51:28 +0500

    Fields Lenta.ru

--- get.py
+++ get.py
@@ -1,15 +1,28 @@
 
 import feedparser
 
-print "Start read RSS Lenta.ru"
+print ("Start read RSS Lenta.ru")
 d = feedparser.parse('https://lenta.ru/rss')
 
-print d['feed']['title']
-print d['feed']['link']
-# print d['feed']['published']
-print d['feed']['description']
-#d.entries[0]
-print len(d.entries)
+print (d.version)
+print (d.feed.title)
+print (d.feed.link)
+print (d.feed.description)
+
+print (len(d.entries))
 
 for item in d.entries:
-    print item['title']
+    print ("-----------------------------------", "\n")
+    print (item.title, "\n", item.title_detail.value, "\n")
+    print ("##########################", "\n")
+    print (item.summary, "\n")
+    print ("##########################", "\n")
+    print (item.summary_detail.value, "\n")
+    print ("##########################", "\n")
+    print (item.published)
+    print ("##########################", "\n")
+    print (item.link)
+
+
+
+    
