418e98bf5c51e616f46de60376b8a0a3884a1986
asd <asd@localhost.localdomain>
2018-05-04 21:14:21 +0500

    xz

--- get.py
+++ get.py
@@ -1,29 +1,32 @@
+#!/usr/bin/env python3
+#
 #Парсер RSS
 #import sys
 #sys.path.append("/usr/local/lib/python3/dist-packages")
 import feedparser
 #Клиент для PostgreSQL
-#import asyncio
+import asyncio
 import asyncpg
 
+async def run():
+    conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='192.168.1.2')
+    values = await conn.fetch('''SELECT * FROM rss_lenta_ru''')
+    await conn.close()
 
-conn = asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
-values = conn.fetch('''SELECT * FROM rss_lenta_ru''')
-conn.close()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
 
-
-
-print ("Start read RSS Lenta.ru")
-url = 'https://lenta.ru/rss'
-rssdates = feedparser.parse(url)
+#print ("Start read RSS Lenta.ru")
+#url = 'https://lenta.ru/rss'
+#rssdates = feedparser.parse(url)
 
 #print (rssdates.modified_parsed)
-print (rssdates.etag)
-last_etag = rssdates.etag
+#print (rssdates.etag)
+#last_etag = rssdates.etag
 
-rssdates = feedparser.parse(url, etag=last_etag)
-if rssdates.status == 304:
-    print ("no changes")
+#rssdates = feedparser.parse(url, etag=last_etag)
+#if rssdates.status == 304:
+#    print ("no changes")
 
 
 #print (rssdates.version)
