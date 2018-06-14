1f04c3b32cf9cabc283d6a46a3cbd222447a8938
asdV <tsuranov@yandex.ru>
2018-06-14 20:52:39 +0500

    goal

--- get.py
+++ get.py
@@ -4,49 +4,36 @@
 #import sys
 #sys.path.append("/usr/local/lib/python3/dist-packages")
 import feedparser
-#Клиент для PostgreSQL
+#асихронный клиент для PostgreSQL
 import asyncio
 import asyncpg
+#official PostgreSQL client library
+import psycopg2
 
-async def run():
+#Получаем список rss-лент
+conn = psycopg2.connect(dbname='webstat', user='webuser', password='!23456',  host='10.10.10.10')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM rss_url")
+url_list = cursor.fetchall()
+conn.close()
+
+#Получаем rss 
+async def get_rss(rss):
+    #Получение rss
+
+    #Запись в PostgreSQL
     conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
-    values = await conn.fetch('''SELECT * FROM rss_lenta_ru''')
+    values = await conn.fetch("SELECT * FROM " + rss[1]) 
     await conn.close()
 
+async def async_get_rss():
+    tasks = [asyncio.ensure_future(
+        get_rss( url_list[i] )) for i in range(0, 1)]
+    await asyncio.wait(tasks)
+
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
-
-#print ("Start read RSS Lenta.ru")
-#url = 'https://lenta.ru/rss'
-#rssdates = feedparser.parse(url)
-
-#print (rssdates.modified_parsed)
-#print (rssdates.etag)
-#last_etag = rssdates.etag
-
-#rssdates = feedparser.parse(url, etag=last_etag)
-#if rssdates.status == 304:
-#    print ("no changes")
-
-
-#print (rssdates.version)
-#print (d.feed.title)
-#print (d.feed.link)
-#print (d.feed.description)
-
-#print (len(d.entries))
-
-#for item in d.entries:
-#    print ("-----------------------------------", "\n")
-#    print (item.title, "\n", item.title_detail.value, "\n")
-#    print ("##########################", "\n")
-#    print (item.summary, "\n")
-#    print ("##########################", "\n")
-#    print (item.summary_detail.value, "\n")
-#    print ("##########################", "\n")
-#    print (item.published)
-#    print ("##########################", "\n")
-#    print (item.link)
+loop.run_until_complete(async_get_rss())
+loop.close()
 
 
 
