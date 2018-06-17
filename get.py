2d14e839a32dc2b8681c5bd6e777ba6889878df5
asd <asd@localhost.localdomain>
2018-06-18 00:06:37 +0500

    work

--- get.py
+++ get.py
@@ -17,13 +17,22 @@ cursor.execute("SELECT * FROM rss_url")
 url_list = cursor.fetchall()
 conn.close()
 
-#Получаем rss 
+#Получаем rss
+# rss[0] id
+# rss[1] таблица
+# rss[2] url
+# rss[3] last_etag
 async def get_rss(rss):
     #Получение rss
+    rssdates = feedparser.parse(rss[2], etag=rss[3])
+    if rssdates.status == 304:
+        return ("no changes")
 
     #Запись в PostgreSQL
     conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
-    values = await conn.fetch("SELECT * FROM " + rss[1]) 
+    for item in rssdates.entries:
+        values = await conn.execute ("INSERT INTO $1 (title, summary, published, link) VALUES ($2, $3, $4)", 
+            rss[1], item.title, item.summary, item.published, item.link) 
     await conn.close()
 
 async def async_get_rss():
