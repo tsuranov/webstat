fecb402f2fb259ef6048288fa544bcd99df68ea6
asdV <tsuranov@yandex.ru>
2018-08-01 19:42:43 +0500

    try & catch

--- get_rss.py
+++ get_rss.py
@@ -35,27 +35,39 @@ async def get_rss(rss):
     try:
         #Запись в PostgreSQL
         conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
+        print (rss[1] + ": connect pg ok")
         #Дата последней новости
         #
-        last_published = await conn.fetchrow('SELECT MAX( published ) FROM ' + rss[1] + ' ;')
+        last_published = (await conn.fetchrow('SELECT MAX( published ) FROM ' + rss[1] + ' ;'))[0]
+        print (rss[1] + ": fetch last_p ok")
         #
         for item in rssdates.entries:
             #Парсим время публикации в формат MySQL
             published = datetime.datetime.strptime(item.published, "%a, %d %b %Y %H:%M:%S %z")
-            if published > last_published[0]:
-                await conn.execute ('INSERT INTO ' + rss[1] + ' (title, published, link) VALUES ($1, $2, $3)', item.title, published, item.link) 
-            else:
-                print(rss[1], published)
+            #if published > last_published[0]:
+                #await conn.execute ('INSERT INTO ' + rss[1] + ' (title, published, link) VALUES ($1, $2, $3)', item.title, published, item.link) 
+            #else:
+                #print(rss[1], published)
         #await conn.execute ('UPDATE rss_url SET etag=$1 WHERE name=$2', rssdates.etag, rss[1]) 
         print ("Insert ===>", rss[1])
         await conn.close()
     
     except asyncio.TimeoutError as error:
-        print(f'{rss[1]}: WEBSOCKET_TIMEOUT: {error}')
+        print(rss[1] + ": WEBSOCKET_TIMEOUT: " + error)
     except (asyncio.CancelledError, KeyboardInterrupt, SystemExit) as error:
-        print(f'{rss[1]}: TASK_CANCELLED: {error}')
+        print(rss[1] + ": TASK_CANCELLED: " + error)
+    except asyncpg.ConnectionDoesNotExistError:
+        pass
+    except (asyncpg.CannotConnectNowError, asyncpg.PostgresConnectionError):
+        print(rss[1] + ": CONNECTION ERROR")
+    except asyncpg.PostgresError as error: 
+        print(rss[1] + ": other error")
+    except OSError:
+        print(rss[1] + ": OSError")
     except Exception as error:
-        print(f'{rss[1]}: {error}')
+        print(rss[1] + ": " + error)
+    else:
+        print(rss[1] + ": ok!")
     finally:
         return 0
 
