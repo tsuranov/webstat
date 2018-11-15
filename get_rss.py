8edb7a88b4129cd68a44c3b379f467c211a72627
asdV <tsuranov@yandex.ru>
2018-11-15 13:14:46 +0500

    work with all rss

--- get_rss.py
+++ get_rss.py
@@ -12,45 +12,41 @@ import psycopg2
 #
 import time
 import datetime
-#Получаем список rss-лент
-conn = psycopg2.connect(dbname='webstat', user='webuser', password='!23456',  host='10.10.10.10')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM rss_url")
-url_list = cursor.fetchall()
-conn.close()
 
-#Получаем rss
-# rss[0] id
-# rss[1] таблица   
-# rss[2] url
-# rss[3] last_etag
-async def get_rss(rss):
-    #Получение rss
-    rssdates = feedparser.parse(rss[2], etag=rss[3])
-    print ("Recive ===>", rss[1])
 
-    if rssdates.status == 304:
-        return ("no changes")
-
+async def get_rss():
     try:
-        #Запись в PostgreSQL
-        conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
-        print (rss[1] + ": connect pg ok")
-        #Дата последней новости
-        #
-        last_published = (await conn.fetchrow('SELECT MAX( published ) FROM ' + rss[1] + ' ;'))[0]
-        print (rss[1] + ": fetch last_p ok")
-        #
-        for item in rssdates.entries:
-            #Парсим время публикации в формат MySQL
-            published = datetime.datetime.strptime(item.published, "%a, %d %b %Y %H:%M:%S %z")
-            #if published > last_published[0]:
-                #await conn.execute ('INSERT INTO ' + rss[1] + ' (title, published, link) VALUES ($1, $2, $3)', item.title, published, item.link) 
-            #else:
-                #print(rss[1], published)
-        #await conn.execute ('UPDATE rss_url SET etag=$1 WHERE name=$2', rssdates.etag, rss[1]) 
-        print ("Insert ===>", rss[1])
-        await conn.close()
+        con = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
+        #Получаем список rss-лент
+        # rss[0] id
+        # rss[1] таблица   
+        # rss[2] url
+        # rss[3] last_etag
+        url_list = await con.fetch('''SELECT * FROM rss_url;''')
+        for rss in url_list:
+            print ("Get rss: " + rss[1])
+            rssdates = feedparser.parse(rss[2], etag=rss[3])
+            print ("Recive rss " + rss[1])
+
+            if rssdates.status == 304:
+                return ("no changes")
+
+            #Дата последней новости в БД
+            last_published = (await con.fetchrow('''SELECT MAX( published ) FROM ''' + rss[1] + ''' ;'''))[0]
+
+            #
+            i = 0
+            for item in rssdates.entries:
+                #Парсим время публикации в формат MySQL
+                published = datetime.datetime.strptime(item.published, "%a, %d %b %Y %H:%M:%S %z")
+                if published > last_published:
+                    #await conn.execute ('INSERT INTO ' + rss[1] + ' (title, published, link) VALUES ($1, $2, $3)', item.title, published, item.link) 
+                    i += 1
+                else:
+                    print(rss[1], published)
+            #await conn.execute ('UPDATE rss_url SET etag=$1 WHERE name=$2', rssdates.etag, rss[1]) 
+            print ("Insert news "  +  str(i) ) #+ " in db ===>" + rss[1])
+
     
     except asyncio.TimeoutError as error:
         print(rss[1] + ": WEBSOCKET_TIMEOUT: " + error)
@@ -59,9 +55,9 @@ async def get_rss(rss):
     except asyncpg.ConnectionDoesNotExistError:
         pass
     except (asyncpg.CannotConnectNowError, asyncpg.PostgresConnectionError):
-        print(rss[1] + ": CONNECTION ERROR")
+        print(rss[1] + ": Postgres CONNECTION ERROR")
     except asyncpg.PostgresError as error: 
-        print(rss[1] + ": other error")
+        print(rss[1] + ": Postgres other error")
     except OSError:
         print(rss[1] + ": OSError")
     except Exception as error:
@@ -69,18 +65,19 @@ async def get_rss(rss):
     else:
         print(rss[1] + ": ok!")
     finally:
+        await con.close()
         return 0
 
 #
-async def async_get_rss():
-    tasks = [asyncio.ensure_future(
-        get_rss( url_list[i] )) for i in range(0, 8)]
-    await asyncio.wait(tasks)
+
+#async def async_get_rss():
+ #   tasks = [asyncio.ensure_future(
+ #       get_rss( url_list[i] )) for i in range(0, len(url_list))]
+ #   await asyncio.wait(tasks)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(async_get_rss())
+loop.run_until_complete(get_rss())
 loop.close()
-
 
 
     
