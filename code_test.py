784d9230da4486eb0e654703e189004ce7b10775
asdV <tsuranov@yandex.ru>
2018-11-21 19:11:23 +0500

    next logging

--- code_test.py
+++ code_test.py
@@ -1,19 +1,71 @@
 #!/usr/bin/env python3
-#
-#Парсер RSS
-#import sys
-#sys.path.append("/usr/local/lib/python3/dist-packages")
-import feedparser
-#асихронный клиент для PostgreSQL
-import asyncio
-import asyncpg
+#######################################################################
+#Логирование простое
+#######################################################################
+import logging
 
-async def run():
-    conn = await asyncpg.connect(user='webuser', password='!23456', database='webstat', host='10.10.10.10')
-    values = await conn.fetch('''SELECT * FROM rss_lenta_ru''')
-    await conn.close()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
-
+# add filemode="w" to overwrite
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+ 
+logging.debug("This is a debug message")
+logging.info("Informational message")
+logging.error("An error has happened!")
+#######################################################################
+#Логирование правильное
+# https://python-scripts.com/logging-python
+# https://docs.python.org/3/howto/logging.html
+# https://rtfm.co.ua/python-loggirovanie-s-pomoshhyu-modulya-logging/
+####################################################################
+import logging
+import otherMod2
+ 
+def main():
+    """
+    The main entry point of the application
+    """
     
+    logger = logging.getLogger("exampleApp")
+    logger.setLevel(logging.INFO)
+    
+    # create the logging file handler
+    fh = logging.FileHandler("new_snake.log")
+ 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    
+    # add handler to logger object
+    logger.addHandler(fh)
+    
+    logger.info("Program started")
+    result = otherMod2.add(7, 8)
+    logger.info("Done!")
+ 
+if __name__ == "__main__":
+    main()
+#######################################################################
+# otherMod2.py
+import logging
+ 
+module_logger = logging.getLogger("exampleApp.otherMod2")
+ 
+def add(x, y):
+    """"""
+    logger = logging.getLogger("exampleApp.otherMod2.add")
+    logger.info("added %s and %s to get %s" % (x, y, x+y))
+    return x+y
+#######################################################################
+# В syslog
+#######################################################################
+import logging
+import logging.handlers
+
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.DEBUG)
+
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+
+my_logger.addHandler(handler)
+
+my_logger.debug('this is debug')
+my_logger.critical('this is critical')
+
