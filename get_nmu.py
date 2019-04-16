4401940bf44cee05d0e2be876944fde4e93b1f51
asdV <tsuranov@yandex.ru>
2019-04-16 19:04:41 +0500

    nmu

---
+++ get_nmu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+##################################################################
+# Парсер по НМУ
+##################################################################
+# BeautifulSoup
+# lxml
+# HTMLParser simple HTML parser
+# **html5lib
+# 
+# Должна быть обработка www.website_to_scrape.com/robots.txt
+#
+#
+# https://techrocks.ru/2018/03/22/web-scraping-python-tutorial/
+#
+
+
+
+
+
+# get raw html
+
+
+# structured data
+
+
